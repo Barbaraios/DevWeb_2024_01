4b7e4b5a73363e198f7520a06fb66b4b35fa1221
Bárbara <164105675+Barbaraios@users.noreply.github.com>
2024-04-30 21:40:52 -0300

    Add files via upload

---
+++ atividade.js
@@ -0,0 +1,27 @@
+// Operadores de comparação e lógico
+console.clear();
+let nome =  Meu nome é Bárbara possuo altura de 1.7 e peso 40, meu IMC é ;
+let altura = 1.7;
+let peso = 40;
+let v = false;
+console.log(teste1 > teste2); // Operador de comparação
+console.log(teste1 < teste2); // Operador de comparação
+console.log(teste1 && teste1); // Operador lógico
+console.log(valor1 && valor2); // Operador lógico
+
+
+
+
+
+
+
+// Operadores de comparação e lógico
+console.clear();
+let teste1 = 1;
+let teste2 = 0;
+let valor1 = true;
+let valor2 = false;
+console.log(teste1 > teste2); // Operador de comparação
+console.log(teste1 < teste2); // Operador de comparação
+console.log(teste1 && teste1); // Operador lógico
+console.log(valor1 && valor2); // Operador lógico
