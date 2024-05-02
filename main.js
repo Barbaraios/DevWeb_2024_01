44664dd9a5dfabd6014eb66de0f560bf1db8351b
Bárbara <164105675+Barbaraios@users.noreply.github.com>
2024-05-02 20:49:16 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,46 +1,70 @@
-console.log('Hello World!');
-console.error('Essa é uma mensagem de erro');
-console.warn('Essa é uma mensagem de aviso!');
-
-// let, const
-console.clear();
-let idade = 30;
-const nome = 'Nico';
-console.log(nome);
-console.log(idade);
-idade = 31;
-console.log(idade);
-
-// String, Numbers, Boolean, null, undefined
-console.clear();
-const nome_pessoa = 'John';
-const idade1 = 30;
-const rating = 4.5;
-const isCool = true;
-const x = null;
-const y = undefined;
-let z;
-console.log(typeof nome_pessoa); // Type of const
-console.log(typeof idade1);
-console.log(typeof rating);
-console.log(typeof isCool);
-console.log(typeof x);
-console.log(typeof y);
-console.log(typeof z);
-
 // Concatenar strings
-console.clear();
-const pessoa = 'Irmão do Jorel';
-const idade2 = 30;
-console.log('Meu nome é ' + pessoa + ' e eu tenho ' + idade2 + ' anos!');
+let valor01 = 100;
+const valor02 = 5;
+console.log('A multiplicação de ' + valor01 +' por ' + valor02 + ' é ' + valor01 *
+valor02 );
+console.log(`A multiplicação de ${valor01} por ${valor02} é ${valor01 * valor02}`);
 
-// Operadores de comparação e lógico
+// Métodos de strings
 console.clear();
-let teste1 = 1;
-let teste2 = 0;
-let valor1 = true;
-let valor2 = false;
-console.log(teste1 > teste2); // Operador de comparação
-console.log(teste1 < teste2); // Operador de comparação
-console.log(teste1 && teste1); // Operador lógico
-console.log(valor1 && valor2); // Operador lógico
+const string01 = 'Instituto da Oportunidade Social';
+// Acessando o primeiro caractere
+console.log(string01.charAt(0));
+console.log(string01[0]);
+// Acessando o décimo primeiro caractere
+console.log(string01.charAt(10));
+console.log(string01[10]);
+
+// Tamanho de strings
+console.clear();
+let texto = 'abcdefehijklmnopqrstuvxwyz';
+console.log(texto.length);
+
+console.clear();
+const texto2 = 'Também 123 [] é uma strings 231 -1\n';
+console.log(texto2.length);
+
+// Maiusculo e minúsculo
+console.clear();
+const s = 'Hello World!';
+console.log(s.toUpperCase());
+console.log(s.toLowerCase());
+
+const str = 'Mozilla';
+console.log(str.substring(1, 3));
+// Saída esperada: "oz"
+console.log(str.substring(2));
+// Saída esperada: "zilla"
+
+const str1 = 'A raposa é um animal esperto';
+const palavras = str1.split(' ');
+console.log(palavras[3]);
+// Saída esperada: "um"
+const chars = str1.split('');
+console.log(chars[7]);
+// Saída esperada: "a"
+const strCopy = str1.split();
+console.log(strCopy);
+// Saída esperada: Array ["A raposa é um animal esperto"]
+
+// Substituir string
+console.clear();
+let mensagem = 'Venha para a Microsoft!';
+console.log(mensagem);
+let novaMensagem = mensagem.replace('Microsoft', 'IOS');
+console.log(novaMensagem);
+
+// Métodos de busca en strings
+console.clear();
+let frase = 'Sou um aluno do IOS e o IOS é muito bom!';
+console.log(frase.indexOf('IOS')); // Returns 16
+console.log(frase.lastIndexOf('IOS')); // Returns 24
+console.log(frase.search('IOS')); // Returns 16
+
+
+console.log(frase.startsWith('S')); // Verdadeiro - retorna true
+console.log(frase.startsWith('Sou')); // Verdadeiro - retorna true
+console.log(frase.startsWith('a')); // Falso - retorna false
+console.log(frase.endsWith('!')); // Verdadeiro - retorna true
+console.log(frase.endsWith('bom!')); // Verdadeiro - retorna true
+console.log(frase.endsWith('x')); // Falso - retorna false
