3bdc6b9773c8dc70b1028f16f938c86161665116
Bárbara <164105675+Barbaraios@users.noreply.github.com>
2024-05-07 21:50:06 -0300

    Add files via upload

---
+++ atividadeLaçosDeRepetição.js
@@ -0,0 +1,30 @@
+console.log ('--números de 1 até 50--')
+// while
+let contador = 1;
+while (contador <= 50) {
+console.log(`Valor = ${contador}`);
+contador++;
+}
+
+
+
+console.log ('--contagem regressiva--')
+// do-while
+let i = 0,
+text = '';
+do {
+73
+text += `O número  ${i}\n`;
+i++;
+} while (i < 10);
+console.log(text);
+
+
+
+console.log ('--números pares de 1 á 100--')
+// For
+for (let i = 0; i < 10; i++) console.log(`se for número par: ${i}`);
+
+// For - Decremento
+for (let i = 10; i > 0; i--) console.log(`Laço For número: ${i}`);
+
