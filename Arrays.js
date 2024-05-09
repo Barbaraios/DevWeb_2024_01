87c77186a0257c78dcccc383e809fcce5c821210
Bárbara <164105675+Barbaraios@users.noreply.github.com>
2024-05-08 21:37:04 -0300

    Add files via upload

---
+++ Arrays.js
@@ -0,0 +1,29 @@
+
+
+
+
+console.clear();
+const num1 =new Array(30,-1,5,3,121);
+const num2 =(-2, 40,16,111,33,164);
+console.log(num1);
+console.log(num2);
+ acessadoelementosdivessosdoarray:
+ console.log('primeiro elemento de num1: ${num1[0]}');
+ console.log('primeiro elemento de num2: ${num2[0]}');
+
+ console.log('primeiro elemento de num1: &{num[2]}');
+ console.log('primeiro elemento de num2: &{num[5]}');
+
+
+ //Arrays bidimensionais
+console.clear();
+const matrix = [
+['Banana', 'Maçã', 'Pêra'],
+['Laranja', true, 1],
+[null, 'Uva', -350],
+];
+// Acessando elementos diversos dos arrays
+console.log(`Acessa a primeira linha: ${matrix[0]}`);
+console.log(`Acessa o segundo elemento: ${matrix[0][1]}`);
+
+
